e2808cce22908d2ac34ec3bbcdc1f2f06eb23ac4
Akimba-png <akimba@gmail.com>
2020-12-07 17:11:18 +0300

    Внесены правки в название классов используемых в скрипте

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector(".js-main-navigation");
-const menuToggle = menu.querySelector(".js-main-navigation__navigation-toggle");
+const menuToggle = menu.querySelector(".js-main-navigation-toggle");
 
-menu.classList.remove("js-main-navigation--no-js");
+menu.classList.remove("main-navigation--no-js");
 menuToggle.addEventListener("click", function () {
-  menu.classList.toggle("js-main-navigation--opened");
+  menu.classList.toggle("main-navigation--opened");
 });
